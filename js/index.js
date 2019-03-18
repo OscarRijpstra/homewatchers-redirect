8a0c8c12bf1c905724fe407a34d99997f448bd1e
Oscar Rijpstra <00276969@rijnijssel.nl>
2019-03-18 15:42:32 +0100

    fixed IE CSS bugs

--- js/index.js
+++ js/index.js
@@ -1,13 +1,13 @@
-window.onload = footer;
-window.addEventListener('resize', footer);
-document.addEventListener('resize', footer);
+// window.onload = footer;
+// window.addEventListener('resize', footer);
+// document.addEventListener('resize', footer);
 
-function footer(){
-    const navigation = document.getElementsByTagName('header')[0];
-    const main = document.getElementsByTagName('main')[0];
-    const footer = document.getElementsByTagName('footer')[0];
-    if(navigation.clientHeight + main.clientHeight + footer.clientHeight > window.innerHeight) footer.style.position = 'relative';
-    else footer.style.position = 'absolute';
-}
+// function footer(){
+//     const navigation = document.getElementsByTagName('header')[0];
+//     const main = document.getElementsByTagName('main')[0];
+//     const footer = document.getElementsByTagName('footer')[0];
+//     if(navigation.clientHeight + main.clientHeight + footer.clientHeight > window.innerHeight) footer.style.position = 'relative';
+//     else footer.style.position = 'absolute';
+// }
 
 document.getElementById('currentyear').innerText = new Date().getFullYear();
